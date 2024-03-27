df8766acc746ca7535809635e05d311582b79945
Jonnattan Griffiths <jonnattan.griffiths@ionix.cl>
2024-03-26 22:36:20 -0300

    avance en algo

--- compile_maven.sh
+++ compile_maven.sh
@@ -2,8 +2,8 @@
 # Ruta donde está instalado el SDK
 export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk-17.jdk/Contents/Home
 # ac'a poner la ruta donde bajaron el ID
-export MAVEN_HOME=/Users/jonnattangriffiths/herramientas/apache-maven-3.9.2
+export MAVEN_HOME=/Users/jonnattangriffiths/herramientas/apache-maven-3.9.6
 export PATH=$PATH:$MAVEN_HOME/bin
 
 echo "--------- Limpia programa para ser compilado desde CERO"
-mvn clean package
+mvn -e clean package
