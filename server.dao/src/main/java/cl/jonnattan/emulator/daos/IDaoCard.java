85d7712333115f7fc8334d158e2c047797e9aeb6
Jonnattan Griffiths Catalán <jonnattan@gmail.com>
2024-12-17 21:43:53 -0300

    Update IDaoCard.java

--- server.dao/src/main/java/cl/jonnattan/emulator/daos/IDaoCard.java
+++ server.dao/src/main/java/cl/jonnattan/emulator/daos/IDaoCard.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
-import cl.jonnattan.emulator.Card;
+import cl.jonnattan.emulator.interfaces.ICard;
 
 /**
  * Clase DAO de tarjetas
@@ -18,14 +18,14 @@ import cl.jonnattan.emulator.Card;
  * @version 1.0 del 22-06-2020
  * 
  */
-public interface IDaoCard extends CrudRepository<Card, Long> {
+public interface IDaoCard extends CrudRepository<ICard, Long> {
 
-	public Card findByToken(String token);
+	public ICard findByToken(String token);
 
 	@Query("select c from Card c where c.client = :client")
-	public List<Card> findByClient(@Param("client") String client);
+	public List<ICard> findByClient(@Param("client") String client);
 
-	public Card findByCardNumber(String cardNumber);
+	public ICard findByCardNumber(String cardNumber);
 
 	@Modifying
 	@Query("update Card c set c.amount = :amount, c.updatedAt = :date where c.id = :id")
