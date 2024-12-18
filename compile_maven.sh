0da57e226724173e11afc66f9b4887da496e0216
Jonnattan Griffiths <jonnattan@gmail.com>
2024-12-18 12:28:48 +0000

    Archivo de compilacion local

--- compile_maven.sh
+++ compile_maven.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # Ruta donde está instalado el SDK
-export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk-17.jdk/Contents/Home
+export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
 # ac'a poner la ruta donde bajaron el ID
-export MAVEN_HOME=/Users/jonnattangriffiths/herramientas/apache-maven-3.9.2
+export MAVEN_HOME=/home/jonnattan/utils/apache-maven-3.8.6
 export PATH=$PATH:$MAVEN_HOME/bin
 
 echo "--------- Limpia programa para ser compilado desde CERO"
