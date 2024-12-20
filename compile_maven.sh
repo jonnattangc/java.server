a9412f99590dda5310a062fba6516ab48707dfa2
Jonnattan Griffiths <jonnattan.griffiths@ionix.cl>
2024-12-19 22:01:38 -0300

    se actualiza el servicio

--- compile_maven.sh
+++ compile_maven.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Ruta donde está instalado el SDK
-export JAVA_HOME=/Users/jonnattangriffiths/Library/Java/JavaVirtualMachines/corretto-11.0.18/Contents/Home/
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk-17.0.3.1.jdk/Contents/Home
 # ac'a poner la ruta donde bajaron el ID
 export MAVEN_HOME=/Users/jonnattangriffiths/herramientas/apache-maven-3.9.6
 export PATH=$PATH:$MAVEN_HOME/bin
